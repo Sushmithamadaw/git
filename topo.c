4c7d48a54ba9389d1102128e12f6a2b190ba8c45
Sushmithamadaw <98949535+Sushmithamadaw@users.noreply.github.com>
2022-05-28 06:32:43 +0530

    updated

--- topo.c
+++ topo.c
@@ -1,43 +1,38 @@
 #include<stdio.h>
-#include<stdlib.h>
-int k=0;
-int top[10];
-int n,a[10][10];
-int visited[10],count=0;
-void dfs(int v);
+int n,visited[10],count=0,a[10][10],topo[10];
+int topology(int v)
+{
+int w,k=0;
+count++;
+visited[v]=count;
+for(w=0;w<n;w++)
+if(a[v][w]==1 && visited[w]==0)
+topology(w);
+printf("%d\t",v);
+topo[k++]=v;
+}
 void main()
 {
-int i,j;
-printf("Enter the no of vertices");
+int i,j,v;
+printf("Enter the number of vertices:");
 scanf("%d",&n);
-printf("Enter the Adjacency matrix:\n");
+printf("Enter the adjacency matrix\n");
 for(i=0;i<n;i++)
 for(j=0;j<n;j++)
-scanf("%d",&a[i][j]); 
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-printf("%d\t",a[i][j]);
-}
-printf("\n");
-}
-printf("DFS ");
+scanf("%d",&a[i][j]);
+printf("Adjacency matrix\n");
 for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+printf("%d\t",a[i][j]);
+printf("\n");		
+}
+printf("-----Topology order-------------\n");
+for(i=0;i<6;i++)
 visited[i]=0;
 for(i=0;i<n;i++)
 if(visited[i]==0)
-dfs(i);
-printf("Topology sorting");
-for(i=n;i>=0;i--)
-printf("%d\t",top[i]);
-}
-void dfs(int v)
-{
-int w;
-count=count+1;
-visited[v]=count;
-for(w=0;w<n;w++)
-if(visited[w]==0 && a[v][w]==1)
-dfs(w);
-printf("%d\t",v);
-top[k++]=v;
+topology(0);
+printf("\n");
+
 }
