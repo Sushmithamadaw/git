d9bce0391dbcb2db7121a1651b93b329dbd3a1d0
Sushmithamadaw <98949535+Sushmithamadaw@users.noreply.github.com>
2022-06-01 20:12:54 +0530

    new one

--- topo.c
+++ topo.c
@@ -1,38 +1,41 @@
 #include<stdio.h>
-int n,visited[10],count=0,a[10][10],topo[10];
-int topology(int v)
-{
-int w,k=0;
-count++;
-visited[v]=count;
-for(w=0;w<n;w++)
-if(a[v][w]==1 && visited[w]==0)
-topology(w);
-printf("%d\t",v);
-topo[k++]=v;
-}
+int a[10][10],k=0,top[10];
+int visited[10],n,count=0;
+void topology(int);
 void main()
 {
-int i,j,v;
-printf("Enter the number of vertices:");
+int i,j;
+printf("no of vertices are");
 scanf("%d",&n);
-printf("Enter the adjacency matrix\n");
+printf("adjacency matrix are\n");
 for(i=0;i<n;i++)
 for(j=0;j<n;j++)
 scanf("%d",&a[i][j]);
-printf("Adjacency matrix\n");
 for(i=0;i<n;i++)
 {
 for(j=0;j<n;j++)
+{
 printf("%d\t",a[i][j]);
-printf("\n");		
 }
-printf("-----Topology order-------------\n");
-for(i=0;i<6;i++)
+printf("\n");
+}
+printf("\ntopology order");
+for(i=0;i<n;i++)
 visited[i]=0;
 for(i=0;i<n;i++)
 if(visited[i]==0)
 topology(0);
-printf("\n");
-
+for(i=n;i>=0;i--)
+printf("%d\t",top[i]);
+}
+void topology(int v)
+{
+int w;
+count=count+1;
+visited[v]=count;
+for(w=0;w<n;w++)
+if(visited[w]==0&&a[v][w]==1)
+topology(w);
+printf("%d-",v);
+top[k++]=v;
 }
